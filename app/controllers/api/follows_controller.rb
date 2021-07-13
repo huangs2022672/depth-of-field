f7fa554a9b04a1bdb3da44179e4f9214439fed32
shiguang <huangs2022672@gmail.com>
2021-07-13 12:23:43 -0400

    completed controllers for follows and likes, not fully tested

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,13 +1,35 @@
 class Api::FollowsController < ApplicationController
   def show
+    @follow = Follow.find(params[:id])
+    render :show
   end
 
   def index
+    @follows = Follow.includes(:follower, :followee).all
+    render :index
   end
 
   def create
+    @follow = Follow.new(follow_params)
+
+    if @follow.save
+      render :show
+    else
+      render json: @follow.errors.full_messages, status: 401
+    end
   end
 
   def destroy
+    @follow = current_user.followee.find_by(id: params[:id])
+    if @follow && @follow.destroy
+      render :show
+    else
+      render json: @follow.errors.full_messages, status: 401
+    end
+  end
+
+  private
+  def follow_params
+    params.require(:follow).permit(:follower_id, :followee_id)
   end
 end
--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,13 +1,35 @@
 class Api::LikesController < ApplicationController
   def show
+    @like = Like.find(params[:id])
+    render :show
   end
 
   def index
+    @likes = Like.includes(:liker, :photo).all
+    render :index
   end
 
   def create
+    @like = Like.new(like_params)
+
+    if @like.save
+      render :show
+    else
+      render json: @like.errors.full_messages, status: 401
+    end
   end
 
   def destroy
+    @like = current_user.likes.find_by(id: params[:id])
+    if @like && @like.destroy
+      render :show
+    else
+      render json: @like.errors.full_messages, status: 401
+    end
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:liker_id, :photo_id)
   end
 end
