17f387bd3d7de1e2b246b746fe881f2c9b659353
shiguang <huangs2022672@gmail.com>
2021-08-06 16:43:02 -0400

    updated links to open in a new tab/added favicon

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 
 const Footer = () => (
   <footer className="footer-nav">
     <div className="footer-text-links">
       <h3>This site is a clone of </h3>
-      <a href="https://www.flickr.com/">
+      <a href="https://www.flickr.com/" target="_blank">
         <img className="flickr-logo" src={window.flickrLogo} alt="Flickr Logo" />
       </a>
     </div>
     <div className="footer-icon-links">
-      <a href="https://github.com/huangs2022672/depth-of-field"><i className="fa fa-github-square fa-lg"></i></a>
-      <a href="https://www.linkedin.com/school/app-academy/"><i className="fa fa-linkedin-square fa-lg"></i></a>
+      <a href="https://github.com/huangs2022672/depth-of-field" target="_blank"><i className="fa fa-github-square fa-lg"></i></a>
+      <a href="https://www.linkedin.com/in/sammy-huang/" target="_blank"><i className="fa fa-linkedin-square fa-lg"></i></a>
+      <a href="https://angel.co/u/sammy-huang-3" target="_blank"><i className="fa fa-angellist fa-lg"></i></a>
     </div>
   </footer>
 )
