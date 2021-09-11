613d16306b434d7878fbfa571a7fe5b0a7873de7
shiguang <huangs2022672@gmail.com>
2021-09-10 21:25:44 -0400

    update link for personal site

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => (
       </a>
     </div>
     <div className="footer-icon-links">
-      <a href="http://www.sammyhuang.me/" target="_blank"><img className="profile-icon" src={window.profileIcon} alt="profile icon" /></a>
+      <a href="https://www.sammyhuang.dev/" target="_blank"><img className="profile-icon" src={window.profileIcon} alt="profile icon" /></a>
       <a href="https://github.com/huangs2022672/depth-of-field" target="_blank"><i className="fa fa-github-square fa-lg"></i></a>
       <a href="https://www.linkedin.com/in/sammy-huang/" target="_blank"><i className="fa fa-linkedin-square fa-lg"></i></a>
       <a href="https://angel.co/u/sammy-huang-3" target="_blank"><i className="fa fa-angellist fa-lg"></i></a>
